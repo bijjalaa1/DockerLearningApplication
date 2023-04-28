6a3a25babbe9f0e03aaea8d9805210b5916b2443
aravind bijjala <aravindbijjala@MacBook-Pro-4.lan>
2023-04-28 13:22:02 -0400

    initial commit

---
+++ src/main/java/com/aravind/dockerlearning/doeckerlearning/DockerLearningApplication.java
@@ -0,0 +1,13 @@
+package com.aravind.dockerlearning.doeckerlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aravind/dockerlearning/doeckerlearning/DockerLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aravind.dockerlearning.doeckerlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
